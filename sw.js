4df2448fc7a2e4fb11b82035d7d82acc9e231d7b
Jerry Ron Sunny <jerryronsunny@proton.me>
2025-11-18 00:41:33 +0530

    Add new ad script to both main portfolio and livescale

--- sw.js
+++ sw.js
@@ -1,14 +1,6 @@
-// Service Worker for Ad Network Verification
-self.addEventListener('install', function(event) {
-  console.log('Service Worker installing.');
-  self.skipWaiting();
-});
-
-self.addEventListener('activate', function(event) {
-  console.log('Service Worker activating.');
-});
-
-self.addEventListener('fetch', function(event) {
-  // Let the browser do its default thing for all requests
-  return;
-});
+self.options = {
+    "domain": "3nbf4.com",
+    "zoneId": 10201662
+}
+self.lary = ""
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
